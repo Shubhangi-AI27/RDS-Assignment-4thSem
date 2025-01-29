ba6163b0d5bc99aea2cd8c6d20e46443e773cf79
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-01-29 23:44:52 +0530

    Update problem_13.R

--- Assignment_1/problem_13.R
+++ Assignment_1/problem_13.R
@@ -1,5 +1,22 @@
+x <- seq(from=3, to=6, length.out=5)
+x
 
+#  A twofold repetition of the vector c(2,-5.1,-33)
+y <- rep(c(2,-5.1,-33),times=2)
+y
 
+# The value 7/42 + 2
+ z <-7/42+2
+ z
+ # Combine x,y,z
+ result <- c(x,y,z)
+ result
+# Output  [1]   3.000000   3.750000   4.500000   5.250000   6.000000   2.000000  -5.100000 -33.000000   2.000000  -5.100000
+        # [11] -33.000000   2.166667
+
+# 13. Create a new vector as a copy of (10.) by assigning (10.) as is to a newly named object.
+# Using this new copy of (10.), overwrite the first, the fifth to the seventh (inclusive), and the last
+# element with the values 99 to 95 (inclusive), respectively.
 
 new_vector <- result
 new_vector <- 99
