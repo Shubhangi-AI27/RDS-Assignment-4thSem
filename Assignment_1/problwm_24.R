cdc3235cc16af6ca9624ff4fa4e8bad3add88ae9
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-01-30 22:27:20 +0530

    Create problwm_24.R

---
+++ Assignment_1/problwm_24.R
@@ -0,0 +1,9 @@
+replacement_values <- c(-0.5, -200, -0.5)
+
+# Replace the middle three elements (positions 9, 10, and 11)
+result[9:11] <- replacement_values
+ # Print the modified vector
+print(result)
+
+# Output [1]    6.000    7.000    8.000    9.000   10.000   11.000   12.000    5.300   -0.500 -200.000   -0.500  102.000   90.375
+#[14]   78.750   67.125   55.500   43.875   32.250   20.625    9.000
