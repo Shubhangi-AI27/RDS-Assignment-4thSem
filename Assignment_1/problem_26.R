18757905300165843122fd6b1671ba267a149c65
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-01-30 22:35:29 +0530

    Create problem_26.R

---
+++ Assignment_1/problem_26.R
@@ -0,0 +1,7 @@
+# Temperature Conversion Vectorized:
+fahrenheit_temps <- c(32, 68, 14, 0, 113, 104, 198)
+celsius_temps <- (5/9) * (fahrenheit_temps - 32)
+# Print the Celsius temperatures
+print(celsius_temps)
+
+# Output  [1]   0.00000  20.00000 -10.00000 -17.77778  45.00000  40.00000  92.22222
