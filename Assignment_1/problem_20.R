bebe944d0b99e4ba219d596f64dd2f6205e9bc18
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-01-30 13:52:02 +0530

    Update problem_20.R

--- Assignment_1/problem_20.R
+++ Assignment_1/problem_20.R
@@ -8,3 +8,13 @@ repeat_vector
 y <- -5
 y
 
+a <- seq(from=5, to=-11, by=-0.3)
+ a
+#Reverse the result of a
+rev(a)
+length(rev(a))
+
+sequence <- seq(from=200, to=length(rev(a)),length.out=12)
+sequence
+ #Output [1] 200.00000 186.72727 173.45455 160.18182 146.90909 133.63636 120.36364 107.09091  93.81818  80.54545  67.27273
+       # [12]  54.00000
