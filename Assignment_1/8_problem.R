010c421e75442e01099aacf69a754a537796056e
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-01-30 20:28:16 +0530

    Update 8_problem.R

--- Assignment_1/8_problem.R
+++ Assignment_1/8_problem.R
@@ -7,7 +7,7 @@ z
 result <- c(x,y,z)
 result
 #Output    [1]   3.000000   3.750000   4.500000   5.250000   6.000000   2.000000  -5.100000 -33.000000   2.000000  -5.100000
-           [11] -33.000000   2.166667
+         # [11] -33.000000   2.166667
 
 # Store as a third object the values returned by omitting the first and last values of your vector from (6.).
 d <- result[-c(1,12)]
