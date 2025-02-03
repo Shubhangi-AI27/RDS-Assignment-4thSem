6cd6c8007cc05d17ae018a07594a2df01965664b
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-02-03 19:00:13 +0530

    Create 8_problem.R

---
+++ Assignment_2/8_problem.R
@@ -0,0 +1,16 @@
+# Store the following
+
+A <- matrix(data=c(1,2,7),nrow=3, ncol=1)
+A
+# Output [,1]
+#  [1,]    1
+#  [2,]    2
+#  [3,]    7
+
+B <- matrix(data=c(3,4,8),nrow=3,ncol=1)
+B
+# Output  [,1]
+#   [1,]    3
+#   [2,]    4
+#   [3,]    8
+
