5da57ab1e8ca41ab30d275b554be3ecd102c1f95
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-03-09 19:11:00 +0530

    Create problem_12.R

---
+++ Assignment_3/problem_12.R
@@ -0,0 +1,11 @@
+# Convert the character vector c("male", "female", "male", "female") into a factor. Display the factor and its levels.
+
+gender_vector <- c("male", "female", "male", "female")
+gender_factor <- factor(gender_vector)
+print(gender_factor)
+# Output [1] male   female male   female
+#           Levels: female male
+
+print(levels(gender_factor))
+# Output [1] "female" "male"  
+
