97bc3a01f1980f452b064e068f4662535302e325
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2025-03-09 19:36:51 +0530

    Create problem_20.R

---
+++ Assignment_3/problem_20.R
@@ -0,0 +1,10 @@
+# Write an R script that creates a string containing a newline, tab, and a backslash using escape sequences. Print the
+# string to the console using cat().
+
+text <- "Line 1\nLine 2 with a tab:\tTabbed Text\nA backslash: \\"
+cat(text)
+
+# Output 
+# Line 1
+# Line 2 with a tab:	Tabbed Text
+# A backslash: \
